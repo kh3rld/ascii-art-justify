985e6c2fb270363129a60627c14e7be1cb9565a1
khahussein <kherld.hussein@gmail.com>
2024-09-09 12:32:05 +0300

    Refactor(usage): Update default usage message to reflect the current project focus

--- utils/print_usage.go
+++ utils/print_usage.go
@@ -6,3 +6,8 @@ func PrintUsage() {
 	fmt.Print("Usage: go run . [OPTION] [STRING] [BANNER]\n\n")
 	fmt.Println("Example: go run . --align=right something standard")
 }
+
+func OutputUsage() {
+	fmt.Print("Usage: go run . [OPTION] [STRING] [BANNER]\n\n")
+	fmt.Println("Example: go run . --output=<fileName.txt> something standard")
+}
